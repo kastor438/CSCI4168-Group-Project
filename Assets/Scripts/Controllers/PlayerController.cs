55a88759c6ccf65b844e325223e3148ad3b44ed1
Justin MacKinnon <kastor438@hotmail.com>
2022-11-01 19:01:23 -0300

    Work on player controller.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -6,11 +6,14 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private PlayerInput playerInput;
+    private Rigidbody2D RB2D;
+    private Vector2 movementInput;
     public float speed;
-
+    
     void Start()
     {
         playerInput = GameManager.Instance.playerInput;
+        RB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +22,11 @@ public class PlayerController : MonoBehaviour
         if (!GameManager.Instance || !GameManager.Instance.player || !GameManager.Instance.playerInput)
             return;
 
-        Vector2 movementInput = playerInput.actions["Movement"].ReadValue<Vector2>();
+        movementInput = playerInput.actions["Movement"].ReadValue<Vector2>();
+    }
+
+    public void FixedUpdate()
+    {
         if (movementInput.magnitude > 0)
         {
             MovePlayer(movementInput);
@@ -27,12 +34,12 @@ public class PlayerController : MonoBehaviour
         else
         {
             // Not moving
-
+            RB2D.velocity = Vector3.zero;
         }
     }
 
     public void MovePlayer(Vector2 movementInput)
     {
-        gameObject.transform.position += new Vector3(movementInput.x * speed * Time.deltaTime, movementInput.y * speed * Time.deltaTime, 0);
+        RB2D.velocity = movementInput * speed;
     }
 }
