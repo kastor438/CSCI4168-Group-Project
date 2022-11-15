c587af536547fac8ea6d0f49b17a6b91e4ea7873
Kastor438 <Kastor438@hotmail.com>
2022-11-15 12:39:11 -0400

    Tile updates, and beginning dialog UI.

---
+++ Assets/Scripts/ScriptableObjects/NPC.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewNPC", menuName = "NPC")]
+public class NPC : ScriptableObject
+{
+    public string[] npcDialog;
+}
